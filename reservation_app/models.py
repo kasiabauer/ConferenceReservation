21a4877abb5b56359c9a12362c8c7637e6aa2773
bauerka <bauer.kasia@gmail.com>
2021-09-11 20:24:11 +0200

    Added form template for adding rooms

--- reservation_app/models.py
+++ reservation_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ConfRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.SmallIntegerField()
+    projector = models.BooleanField()
+
--- reservation_app/views.py
+++ reservation_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import ConfRoom
+from django.template.response import TemplateResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+
+@csrf_exempt
+def new_room(request):
+    if request.method == "GET":
+        return TemplateResponse(request, 'new-room-form.html')
+    if request.method == 'POST':
+        room_name = request.POST.get('room-name')
+        room_capacity = request.POST.get('room-capacity')
+        if room_name == '':
+            return TemplateResponse(request, 'new-room-form-name-error.html')
+        elif room_capacity == '' or int(room_capacity) < 0:
+            return TemplateResponse(request, 'new-room-form-capacity-error.html')
+        # dopisz: if'a czy stala już istnieje w bazie
+        else:
+            return HttpResponse(request, 'new-room-form.html')
